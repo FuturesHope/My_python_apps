f221074a179cf0601c91c6cf3df35e943d1c9afb
Alex <48167410+FuturesHope@users.noreply.github.com>
2020-08-02 12:59:50 +0100

    Challenge game in python
    Refreshing python, skill up, training.

---
+++ guess_the_num_game.py
@@ -0,0 +1,27 @@
+
+
+import random, sys
+
+#start the game: Describe, tell the rules, ask for the name
+print("Hi, this is the 'Guess the number' game")
+print('You will be given 6 chances to guess a num from 1 to 20 including with the help of hints')
+player_name = input("Please write your name ")
+secret_number = random.randint(1,21)
+print(f"Lets's start the game {player_name}, i picked a number, guess it..")
+#ask for the guessings
+#num_of_guesses=0
+
+for x in range(0,6):
+    try:
+        player_guess=int(input("What's your guess? "))
+        if(player_guess == secret_number):
+            print(f"Yes, you guessed! It's {secret_number}")
+            sys.exit()
+        elif(player_guess<secret_number):
+            print("Go higher..")
+        elif(player_guess>secret_number):
+            print("Take lower..")
+    except Exception:
+        print("An error occured, contact Al..")
+
+print(f"You ran out of 6 chances, the num was {secret_number}")
